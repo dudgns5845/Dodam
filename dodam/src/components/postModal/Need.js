5b99c5ed3f74958960cc93e9f801c8b82ff3bac5
jho951 <jho951@naver.com>
2022-08-29 22:38:52 +0900

    #4 요청해요 페이지 기능 구현

--- dodam/src/components/postModal/Need.js
+++ dodam/src/components/postModal/Need.js
@@ -1,3 +1,95 @@
+import { useState, useRef } from "react";
+
 export default function Need({ closeModal }) {
-  return <>안녕3</>;
+  const [title, setTitle] = useState("");
+  const tagRef = useRef();
+  const [contents, setContents] = useState("");
+  const [imgState, setImageState] = useState("/logo512.png");
+  const [tagList, setTagList] = useState([]);
+
+  const CloseHandler = () => {
+    closeModal();
+  };
+  const ClickHandler = () => {
+    const tag = tagList;
+    console.log(title, tag, contents, imgState);
+  };
+  const TitleChangeHandler = (e) => {
+    const title = e.currentTarget.value;
+    setTitle(title);
+  };
+  const ContentsChangeHandler = (e) => {
+    const contents = e.currentTarget.value;
+    setContents(contents);
+  };
+  const TagInput = () => {
+    if (window.event.keyCode === 13 && tagRef.current.value !== "") {
+      setTagList([...tagList, tagRef.current.value]);
+      tagRef.current.value = "";
+    }
+  };
+  const DeleteTag = (key) => {
+    const newTags = tagList.filter((item, idx) => {
+      return idx !== key;
+    });
+    setTagList(newTags);
+  };
+  const onFileChange = (e) => {
+    setImageState(URL.createObjectURL(e.target.files[0]));
+  };
+  return (
+    <div>
+      <h1>요청하세요</h1>
+      <input
+        type="text"
+        onChange={TitleChangeHandler}
+        value={title}
+        placeholder="가게 제목을 입력해주세요"
+      />
+      <input
+        type="text"
+        ref={tagRef}
+        onKeyUp={TagInput}
+        placeholder="태그를 입력해주세요"
+      />
+      {tagList.map((tag, idx) => (
+        <div
+          key={idx}
+          onClick={() => DeleteTag(idx)}
+          style={{
+            background: "#eee",
+            border: "none",
+            borderRadius: "20px",
+            height: "35px",
+            padding: "5px 15px",
+          }}
+        >
+          {tag}
+        </div>
+      ))}
+      <input
+        style={{ width: "400px", height: "400px" }}
+        type="text"
+        onChange={ContentsChangeHandler}
+        value={contents}
+        placeholder="가게 정보를 입력해주세요"
+      />
+      <img
+        alt="sample"
+        src={imgState}
+        style={{
+          width: "200px",
+          height: "200px",
+        }}
+      />
+      <input
+        type="file"
+        accept="image/*"
+        encType="multipart/form-data"
+        onChange={onFileChange}
+      />
+      <button onClick={ClickHandler}>등록하기</button>
+      <button onClick={CloseHandler}>되돌아가기</button>
+    </div>
+  );
 }
