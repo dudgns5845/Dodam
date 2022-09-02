975e7c1461174b7a17578d2a0fa2148a2579a0ed
sangjin <sasangjin@gmail.com>
2022-09-02 18:58:05 +0900

    login기능 공유

--- dodam/src/pages/Login.js
+++ dodam/src/pages/Login.js
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
+import Snackbar from '@mui/material/Snackbar';
+
 
 
 const ariaLabel = { 'aria-label': 'description' };
@@ -22,14 +24,58 @@ export default function Login(props) {
     const [passwordMessage, setPasswordMessage] = useState('')
     const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('')
 
+    const [errorMessage, setErrorMessage] = useState('')
+
     // 유효성 검사
     const [isUserId, setIsUserId] = useState(false)
     const [isPassword, setIsPassword] = useState(false)
     const [isPasswordConfirm, setIsPasswordConfirm] = useState(false)
 
+    // MUI snackbar
+    const [state, setState] = React.useState({
+        open: false,
+        vertical: 'top',
+        horizontal: 'center',
+      });
+    
+      const { vertical, horizontal, open } = state;
+    
+      const handleClick = (newState) => () => {
+        setState({ open: true, ...newState });
+      };
+    
+      const handleClose = () => {
+        setState({ ...state, open: false });
+    };
+
+    const onSubmitHandler = (e) => {
+        if (!isUserId) { return setErrorMessage('이메일을 확인해주세요') }
+        if (userName.length === 0) { return setErrorMessage('이름을 입력해주세요') }
+        if (!isPassword) { return setErrorMessage('비밀번호를 확인해주세요') }
+        if (!isPasswordConfirm) { return setErrorMessage('비밀번호가 일치하지 않습니다')}
+        if (userBirthDay.length === 0) { return setErrorMessage('생일을 입력해주세요') }
+        if (nickName.length === 0) { return setErrorMessage('닉네임을 입력해주세요') }
+        if (passwordConfirm.length === 0) { return setErrorMessage('비밀번호 확인을 입력해주세요') }
+        e.preventDefault()
+        console.log(body)
+    };
+    
+    const buttons = (
+        <React.Fragment>
+            <Button
+            variant="contained"
+            onClick={onSubmitHandler({
+              vertical: 'top',
+              horizontal: 'center',
+            })}
+            >{auth ? '회원가입' : '로그인'}
+            </Button>
+        </React.Fragment>
+    )
+
     // 이메일 조건 확인
     const onChangeUserId = (e) => {
-        const idRegex = /^[0-9a-zA-Z]([+=-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([+=-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,8}$/;
+        const idRegex = /^[0-9a-zA-Z]([+=-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([+=-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,15}$/;
         const idCurrent = e.target.value
         setUserId(idCurrent)
         if ( !idRegex.test(idCurrent) ) {
@@ -46,7 +92,7 @@ export default function Login(props) {
         const passwordCurrent = e.target.value
         setPassword(passwordCurrent)
         if ( !passwordRegex.test(passwordCurrent) ) {
-            setPasswordMessage('알파벳+숫자+특수문자 조합으로 8자리 이상 입력해주세요')
+            setPasswordMessage('알파벳+숫자+특수문자 조합으로 8자리 이상 15자리 이하로 입력해주세요')
             setIsPassword(false)
         } else {
             setPasswordMessage('안전한 비밀번호 형식입니다')
@@ -88,25 +134,16 @@ export default function Login(props) {
     }
     
     // api 연결용
+    
     // 중복 확인
     const onDoublingHandler = (e) => {
         e.preventDefault()
         console.log(userId)
     };
-    // login or register 제출
-    const onSubmitHandler = (e) => {
-        e.preventDefault()
-        if (!isUserId) { return alert('이메일을 확인해주세요') }
-        if (!isPassword) { return alert('비밀번호를 확인해주세요') }
-        if (!isPasswordConfirm) { return alert('비밀번호가 일치하지 않습니다')}
-        if (userName.length === 0) { return alert('이름을 입력해주세요') }
-        if (userBirthDay.length === 0) { return alert('생일을 입력해주세요') }
-        if (nickName.length === 0) { return alert('닉네임을 입력해주세요') }
-        if (passwordConfirm.length === 0) { return alert('비밀번호 확인을 입력해주세요') }
-        
 
-        console.log(body)
-    };
+    // login or register 제출
+    
+
     // login register 스위치
     const onChangeHandler = (e) => {
         e.preventDefault()
@@ -118,15 +155,16 @@ export default function Login(props) {
     
     return (
         <div style={{margin: "auto", width:"500px", height:"100%", marginTop:"2vh", padding:"50px", boxShadow:"rgba(0, 0, 0, 0.1) 0px 4px 12px"}}>
-            <p>{!auth ? '로그인' : '회원가입'}페이지 입니다.</p>
+            <h2>{!auth ? '로그인' : '회원가입'}</h2>
             <form>
             <p>이메일</p>
             <Input fullWidth placeholder='이메일을 입력하세요' inputProps={ariaLabel} onChange={onChangeUserId}/>
-            {auth && (
+            {auth && 
                 <>
-                <button onClick={onDoublingHandler}>중복확인</button>
+                    <p style={{marginTop:"-10px"}}/>
+                    <Button variant="outlined" onClick={onDoublingHandler}>중복확인</Button>
                 </>
-            )}
+            }
             <br />
             {auth && userId.length > 1 && isUserId ? 
             <span>{userIdMessege}</span>
@@ -161,10 +199,22 @@ export default function Login(props) {
                 </>
                 )}
                 
-            <p />
-            <button onClick={onSubmitHandler}>{auth ? '회원가입' : '로그인' }</button>
+                <p />
+                <div>
+                    {buttons}
+                    <Snackbar
+                        onClick={handleClick}
+                        anchorOrigin={{ vertical, horizontal }}
+                        open={open}
+                        onClose={handleClose}
+                        message={errorMessage}
+                        key={vertical + horizontal}
+                        >
+                    </Snackbar>
+                </div>
+                
             </form>
-            <button onClick={onChangeHandler}>{auth ? '로그인' : '회원가입' }으로 가기</button>
+            <Button variant="text" onClick={onChangeHandler}>{auth ? '로그인' : '회원가입' }으로 가기</Button>
             
         </div>
         
